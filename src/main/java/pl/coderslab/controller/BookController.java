8e2cbd68ca47e94ce72f1217bd464656fa36130c
Damian Mielcarekan <damian.mielcarek@gmail.com>
2018-01-14 16:22:29 +0100

    exercise 6 - done - list of all books

--- src/main/java/pl/coderslab/controller/BookController.java
+++ src/main/java/pl/coderslab/controller/BookController.java
@@ -1,10 +1,15 @@
 package pl.coderslab.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import pl.coderslab.entity.Book;
 import pl.coderslab.service.MemoryBookService;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/books")
@@ -21,5 +26,11 @@ public class BookController {
     public Book helloBook(){
         return new Book(1L,"9788324631766","Thinking in Java",
                 "Bruce Eckel","Helion","programming");
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/list")
+    public Map<Long, Book> getList() {
+        return memoryBookService.getList();
     }
 }
